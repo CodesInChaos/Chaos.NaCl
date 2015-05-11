2e330f002aa7e395ba5f51fe607b261bbc47cc83
Alexey Ermishkin <alexey.ermishkin@engine6.com>
2015-05-11 12:45:23 +0500

    Elligator implementation

---
+++ Chaos.NaCl/Internal/Ed25519Ref10/fe_bytesLE.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Chaos.NaCl.Internal.Ed25519Ref10
+{
+    internal static partial class FieldOperations
+    {
+        public static int fe_bytesLE(ref byte[] a, ref byte[] b)
+        {
+            var equalSoFar = -1;
+            var greater = 0;
+            for (var i = 31; i > 0; i--)
+            {
+                var x = a[i];
+                var y = b[i];
+                greater = (~equalSoFar & greater) | (equalSoFar & ((x - y) >> 31));
+                equalSoFar = equalSoFar & (((x ^ y) - 1) >> 31);
+            }
+            return ~equalSoFar & 1 & greater;
+        }
+    }
+}
